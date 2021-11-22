d38ccea652943e9fb58270f574fdb6690535898f
pkcody <77869251+pkcody@users.noreply.github.com>
2021-11-22 13:48:27 -0700

    made particles work for third selection

--- SOLID Selection Project/Assets/_Project/Scripts/IndicatorSelectionResponse.cs
+++ SOLID Selection Project/Assets/_Project/Scripts/IndicatorSelectionResponse.cs
@@ -8,31 +8,20 @@ public class IndicatorSelectionResponse : MonoBehaviour, ISelectionResponse
 
     public void OnSelect(Transform selection)
     {
-        block.SetActive(true);
-        var particle = block.GetComponent<ParticleSystem>();
-
         if (block != null)
         {
             var height = selection.transform.localScale.y;
             block.transform.position = new Vector3(selection.transform.position.x, height * 2f, selection.transform.position.z);
-            var emission = particle.emission;
 
-            emission.enabled = true;
         }
-
-
     }
 
     public void OnDeselect(Transform selection)
     {
-        var particle = block.GetComponent<ParticleSystem>();
         if (block != null)
         {
-            block.transform.position = selection.transform.position;
-            particle.Stop();
-            block.SetActive(false);
+            block.transform.position = new Vector3(10,-10,10);
 
         }
     }
-
 }
